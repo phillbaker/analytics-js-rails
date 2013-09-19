7e409fb472cbc09caa0d31c53a6880f34edb3bb8
Phill Baker <phillbaker@retrodict.com>
2013-09-18 22:56:01 -0400

    Add helper method for asset url, not in rails.

--- lib/analytics-js-rails.rb
+++ lib/analytics-js-rails.rb
@@ -1,4 +1,13 @@
 require "analytics-js/version"
 require "analytics-js/engine"
 
-module AnalyticsJs; end
+module AnalyticsJs
+  # Unfortunately Rails doesn't provide a helper for this. So this is it.
+  # http://stackoverflow.com/q/7597127
+  def asset_url(asset)
+    root_url[0...-1] + ActionController::Base.helpers.asset_path(asset)
+  end
+end
+
+# ActionController::Base.send :include, AnalyticsJs
+ActionView::Base.send :include, AnalyticsJs
