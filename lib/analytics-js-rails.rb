68635e2210d7b67be9d47aa2808a031a6d34ec6a
Phill Baker <phillbaker@retrodict.com>
2013-09-19 00:20:56 -0400

    Clarify notes for helper method.

--- lib/analytics-js-rails.rb
+++ lib/analytics-js-rails.rb
@@ -2,12 +2,12 @@ require "analytics-js/version"
 require "analytics-js/engine"
 
 module AnalyticsJs
-  # Unfortunately Rails doesn't provide a helper for this. So this is it.
-  # http://stackoverflow.com/q/7597127
+  # Unfortunately Rails doesn't provide a helper for this. So this is it. Can
+  # be overridden in host app.
+  # See http://stackoverflow.com/q/7597127
   def asset_url(asset)
     root_url[0...-1] + ActionController::Base.helpers.asset_path(asset)
   end
 end
 
-# ActionController::Base.send :include, AnalyticsJs
 ActionView::Base.send :include, AnalyticsJs
